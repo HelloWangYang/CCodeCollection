91921235f18e4d2a64ea1afa7d7501d99e5bdab7
WangYang <wangyanghelloworld@163.com>
2014-09-30 16:12:22 +0800

    BM Search (not complete)

---
+++ kmpsearch/bmsearch.c
@@ -0,0 +1,17 @@
+/*=============================================================================
+#     FileName: bmsearch.c
+#         Desc: Boyer-Moore Algorithm
+#       Author: WangYang
+#        Email: whseay@gmail.com
+#     HomePage: http://www.cnblogs.com/whseay/
+#      Version: 0.0.1
+#   LastChange: 2014-08-13 19:10:16
+#      History:
+=============================================================================*/
+
+#include <stdio.h>
+// TODO do it!
+int main(void)
+{
+	return 0;
+}
